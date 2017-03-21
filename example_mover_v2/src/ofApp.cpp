622e5faf830f4e71810d3d9360e30ab9c438e476
Christopher Baker <me@christopherbaker.net>
2017-03-21 01:18:12 -0500

    Update.

---
+++ example_mover_v2/src/ofApp.cpp
@@ -0,0 +1,65 @@
+//
+// Copyright (c) 2017 Christopher Baker <https://christopherbaker.net>
+//
+// SPDX-License-Identifier:	MIT
+//
+
+
+#include "ofApp.h"
+
+
+
+void ofApp::setup()
+{
+    ofSetLoggerChannel(std::make_shared<ofxIO::ThreadsafeConsoleLoggerChannel>());
+
+
+    for (auto line: ofBufferFromFile("folders.txt").getLines()) paths.send(line);
+
+
+    std::size_t numThreads = 8;
+
+    for (std::size_t i = 0; i < numThreads; ++i)
+    {
+        threads.emplace_back(std::thread([&](){
+            std::filesystem::path path;
+            while (paths.receive(path))
+            {
+                try
+                {
+                    std::map<uint64_t, std::vector<std::filesystem::path>> paths;
+
+                    std::filesystem::directory_iterator iter(path), end;
+
+                    while (iter != end)
+                    {
+                        std::string extension = std::filesystem::extension(*iter);
+                        std::string basename = std::filesystem::basename(*iter);
+
+                        if (basename.size() > 0 && basename[0] != '.' && extension != ".gz" && extension != ".json")
+                        {
+                            ofLogNotice() << ">" << iter->path().filename().string();
+                        }
+
+                        ++iter;
+                    }
+
+                }
+                catch (const std::exception& exc)
+                {
+                    ofLogError() << exc.what();
+                }
+
+            }
+        }));
+    }
+
+    // ofx::IO::JSONUtils::saveJSON(jsonPath, Post::toJSON(existingPost));
+}
+
+
+void ofApp::exit()
+{
+    paths.close();
+    for (auto& t: threads) t.join();
+}
